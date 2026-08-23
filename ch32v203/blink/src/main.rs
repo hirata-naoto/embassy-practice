a4a012edf9c31ed7c49f9261ad6368750dd4dafb
hirata-naoto <hkn110@gmail.com>
2026-08-23 13:46:13 +0900

    add ch32v203

---
+++ ch32v203/blink/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+use panic_halt as _;
+use ch32_hal as hal;
+use hal::gpio::{Level, Output};
+use embassy_executor::Spawner;
+use embassy_time::Timer;
+
+
+#[embassy_executor::main(entry = "qingke_rt::entry")]
+async fn main(_spawner: Spawner) {
+    let p = hal::init(hal::Config::default());
+
+    let mut led = Output::new(p.PB0, Level::Low, Default::default());
+
+    loop {
+        led.set_high();
+        Timer::after_millis(500).await; // 500ミリ秒待つ
+        led.set_low();
+        Timer::after_millis(500).await; // 500ミリ秒待つ
+    }
+}
