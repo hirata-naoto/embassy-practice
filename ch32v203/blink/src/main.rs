f720576a6898b9121f681e6e39df4cecd7412e8d
hirata-naoto <hkn110@gmail.com>
2026-08-24 22:46:38 +0900

    add nutton and tx.

--- ch32v203/blink/src/main.rs
+++ ch32v203/blink/src/main.rs
@@ -14,6 +14,16 @@ async fn main(_spawner: Spawner) {
 
     let mut led = Output::new(p.PB2, Level::Low, Default::default());
 
+    // ボタン (PA0) 内部プルアップ、押したらLow想定
+    let button = Input::new(p.PA0, Pull::Up);
+
+
+    // シリアル送信のみ (USART1 TX=PA9)
+    let mut usart_config = UsartConfig::default();
+    usart_config.baudrate = 115200;
+    let mut tx = UartTx::new_blocking(p.USART1, p.PA9, usart_config).unwrap();
+
+
     loop {
         led.set_high();
         Timer::after_millis(500).await; // 500ミリ秒待つ
