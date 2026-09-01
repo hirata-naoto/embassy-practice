5b9676488822b964441b805a5d20d3a14f962013
hirata-naoto <hkn110@gmail.com>
2026-09-02 00:10:00 +0900

    move linker settings to build.rs

---
+++ ch32v203/blink/build.rs
@@ -0,0 +1,8 @@
+fn main() {
+    println!("cargo:rustc-link-search=.");
+    println!("cargo:rerun-if-changed=memory.x");
+    println!("cargo:rerun-if-changed=build.rs");
+
+    println!("cargo:rustc-link-arg=-Tlink.x");
+    println!("cargo:rustc-link-arg=-Tdefmt.x");
+}
