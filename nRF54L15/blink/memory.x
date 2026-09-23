fd522b76c1f697b11286f46e10233c9f0d266137
hirata-naoto <hkn110@gmail.com>
2026-09-23 15:57:31 +0900

    Fix for secure target

--- nRF54L15/blink/memory.x
+++ nRF54L15/blink/memory.x
@@ -1,10 +1,7 @@
-/* XIAO nRF54L15 (nrf54l15-app-ns) 用 memory.x */
 MEMORY
 {
-  /* Non-Secure 領域の Flash と RAM の開始アドレスとサイズ */
   FLASH : ORIGIN = 0x00000000, LENGTH = 1524K
   RAM   : ORIGIN = 0x20000000, LENGTH = 256K
 }
 
-/* スタック割り当ての設定 */
 _stack_start = ORIGIN(RAM) + LENGTH(RAM);
--- nRF54L15/blink/src/main.rs
+++ nRF54L15/blink/src/main.rs
@@ -5,7 +5,7 @@ use core::{default, ops::DerefMut};
 use defmt::*;
 use defmt_rtt as _;
 use panic_probe as _;
-use embassy_nrf::gpio::{Flex, Level, Input, Output, Pull, OutputDrive};
+use embassy_nrf::gpio::{Level, Input, Output, Pull, OutputDrive};
 use embassy_executor::Spawner;
 use embassy_time::Timer;
 
@@ -17,16 +17,18 @@ async fn main(_spawner: Spawner) {
     let config = embassy_nrf::config::Config::default();
     let p = embassy_nrf::init(config);
 
-    let mut led = Output::new(p.P2_00, Level::High, OutputDrive::Standard);
+    let mut led = Output::new(p.P2_00, Level::Low, OutputDrive::Standard);
 
     let button = Input::new(p.P0_00, Pull::Up);
 
     loop {
         info!("LED on");
-        led.set_high();
+        led.set_low();
 
         Timer::after_millis(500).await;
         info!("LED off");
-        led.set_low();
+        led.set_high();
+        Timer::after_millis(500).await;
+
     }
 }
